5aa8a4fd55e602a09eeb3a757d83ace3cd60a1ee
vamshi9897 <vamshibathula9897@gmail.com>
2025-02-24 19:11:59 +0530

    commit

--- files/transporter.js
+++ files/transporter.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    secure: true,
     auth: {
         user: process.env.EMAIL, // Your email
         pass: process.env.PASSWORD // Your email password or app password
